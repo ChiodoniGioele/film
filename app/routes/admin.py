578e75f9fbcaeee2f2dcd14baccfcfbaf26106d2
ChiodoniGioele <gioele.chiodoni@samtrevano.ch>
2024-09-23 14:28:25 +0200

    login

--- app/routes/admin.py
+++ app/routes/admin.py
@@ -1,15 +1,12 @@
 from flask import Blueprint
 from flask_login import login_required, current_user
-from ..decotators import user_has_role
+from ..helper.decotators import user_has_role
 
 admin_bp = Blueprint('admin', __name__)
 
 @admin_bp.route('/')
+@login_required
+@user_has_role('admin')
 def default():
     return 'Admin Home'
 
-@admin_bp.route('/dashboard')
-@login_required
-@user_has_role('admin')
-def dashboard():
-    return 'Admin Dashboard'
