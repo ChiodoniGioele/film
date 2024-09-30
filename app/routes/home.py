954e89c1c6a28860adb80b57056688a76a0a33c7
ChiodoniGioele <gioele.chiodoni@samtrevano.ch>
2024-09-30 11:16:54 +0200

    palinsesto

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,9 +1,12 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
+from app.models.program import *
 
 home_bp = Blueprint('home', __name__)
 
 @home_bp.route('/')
 @login_required
 def home():
-    return render_template('home/home.html', name=current_user.username)
+    data = getData(0)
+    programs = getPrograms(data)
+    return render_template('home/home.html', programs=programs)
